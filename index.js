1ee7934fa8a98fc5308e6b3765a9b3a730300480
Sparetire <s9ar371r3@gmail.com>
2017-04-28 02:01:34 +0800

    add config support

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const path = require('path');
 const run = require('supervisor/lib/supervisor').run;
 const argv = yargs.usage('Usage: oock [options] -d directory')
 	.alias('d', 'directory')
+	.alias('c', 'config')
 	.alias('w', 'watch')
 	.alias('p', 'port')
 	.alias('h', 'host')
 	.describe('d', 'directory of controller and model')
+	.describe('c', 'config file of oock')
 	.describe('w',
 		'when a change to a js file or json file occurs, reload the server. Default is false'
 	)
 	.describe('p', 'port of mock server')
 	.describe('h', 'host of mock server')
-	.demandOption(['d'])
+	// .demandOption(['d'])
 	.help('help')
 	.example('oock -w -p 80 -h 127.0.0.1 -d ./model')
 	.epilog('By Sparetire')
